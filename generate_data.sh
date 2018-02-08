3f072813194144bde802d07efd3e1cda702a3028
Henry Dang <henrydangprg@gmail.com>
2018-02-08 12:41:52 -0500

    Separate data into train_data directory

--- generate_data.sh
+++ generate_data.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-rm -rf tide_ads
-rm -rf non_tide_ads
+rm -rf train_data
+mkdir train_data
+cd train_data
 
 mkdir tide_ads
 mkdir non_tide_ads
-cd tide_ads && ffmpeg -i ../tide.avi -vf scale=512:288 -r 5 -f image2 image-%07d.png
-cd ../non_tide_ads && ffmpeg -i ../non_tide.avi -vf scale=512:288 -r 5 -f image2 image-%07d.png
+
+cd tide_ads && ffmpeg -i ../../tide.avi -vf scale=512:288 -r 5 -f image2 tide-%07d.png
+cd ../non_tide_ads && ffmpeg -i ../../non_tide.avi -vf scale=512:288 -r 5 -f image2 nottide-%07d.png
