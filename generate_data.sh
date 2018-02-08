096317814083088f7c10c41cc006916036f1717e
Henry Dang <henrydangprg@gmail.com>
2018-02-08 11:53:06 -0500

    Rescale images in generate_data.sh to be tinier

--- generate_data.sh
+++ generate_data.sh
@@ -5,5 +5,5 @@ rm -rf non_tide_ads
 
 mkdir tide_ads
 mkdir non_tide_ads
-cd tide_ads && ffmpeg -i ../tide.avi -r 5 -f image2 image-%07d.png
-cd ../non_tide_ads && ffmpeg -i ../non_tide.avi -r 5 -f image2 image-%07d.png
+cd tide_ads && ffmpeg -i ../tide.avi -vf scale=512:288 -r 5 -f image2 image-%07d.png
+cd ../non_tide_ads && ffmpeg -i ../non_tide.avi -vf scale=512:288 -r 5 -f image2 image-%07d.png
